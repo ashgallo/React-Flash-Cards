905e522c0005d583a38564ed19d565391965cf74
Ashley Margallo <amarie921@yahoo.com>
2018-03-08 09:21:01 -0700

    initial flash cards

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import Card from './Card';
+
+class App extends Component {
+  state = { 
+    card: [question, answer],
+  }
+
+
+  render() {
+    const { }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Flash Cards</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const styles = {
+}
+
+const Card = ({ question, answer }) => (
+  onClick=
+)
